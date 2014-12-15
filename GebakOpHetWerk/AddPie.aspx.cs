d52c1d043b08d762e388102ed3c7b4ef07b9e15d
petervanhummel <petervanhummel@hotmail.com>
2014-12-15 11:20:39 +0100

    add pie update

--- GebakOpHetWerk/AddPie.aspx.cs
+++ GebakOpHetWerk/AddPie.aspx.cs
@@ -14,6 +14,9 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void btnSave_Click(object sender, EventArgs e)
     {
+        GebakophetWerkEntities gb = new GebakophetWerkEntities();
+        Pie taart = new Pie();
+
         if (fuPicture.HasFile)
         {
             try
@@ -23,20 +26,29 @@ public partial class _Default : System.Web.UI.Page
                     if (fuPicture.PostedFile.ContentLength < 102400)
                     {
                         string filename = Path.GetFileName(fuPicture.FileName);
-                        fuPicture.SaveAs(Server.MapPath("~/") + filename);
-                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: File uploaded!"+"');", true);
+                        fuPicture.SaveAs(Server.MapPath("~/Images/Cakes") + filename);
+                        taart.Picture = "~/Images/Cakes" + filename;
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: File uploaded!" + "');", true);
                     }
                     else
-                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" +"Upload status: The file has to be less than 100 kb!" +"');", true);                       StatusLabel.Text = ;
+                        ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: The file has to be less than 100 kb!" + "');", true);
                 }
                 else
-                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: Only JPEG files are accepted!" + "');", true);                    StatusLabel.Text = ;
+                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: Only JPEG files are accepted!" + "');", true);
             }
             catch (Exception ex)
             {
                 ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Upload status: The file could not be uploaded. The following error occured: " + ex.Message + "');", true);
+
             }
         }
 
+        taart.Name = tbPiename.Text;
+        taart.Price = Convert.ToInt32(tbPrice.Text);
+        taart.Description = tbDescription.Text;
+        taart.Activated = true;
+
+        gb.Pies.Add(taart);
+        gb.SaveChanges();
     }
 }
